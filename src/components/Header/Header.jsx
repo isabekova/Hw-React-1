d2c0f583ac9f27e63209f4a8104b2ec9f68684d9
Nurkyz Isabekova <nurkyza90@gmail.com>
2022-05-17 15:20:14 +0600

    new addition

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css';
+
+
+export default function Header() {
+       return(
+              <div className='header'>
+                            <div>Туры</div>
+                            <div>Горящие туры</div>
+                            <div>Страны</div>
+                            <div>Визы</div>
+              </div>
+       )
+}
